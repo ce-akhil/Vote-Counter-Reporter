2f9fdca754818b63aa8676c907f6d850aa7b5e20
Akhil Ruhil <akhilruhil11@gmail.com>
2020-03-30 17:52:53 +0530

    Voting Options

--- lib/voting_options.dart
+++ lib/voting_options.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:responsive_grid/responsive_grid.dart';
 import 'package:vcr/action_button.dart';
 import 'package:vcr/appbottombar.dart';
-import 'package:vcr/contactus.dart';
-import 'package:vcr/profile.dart';
-import 'package:vcr/upcoming_elections.dart';
-import 'package:vcr/my_votes.dart';
 
 class VotingOption extends StatefulWidget {
   @override
@@ -25,128 +20,35 @@ class _VotingOptionState extends State<VotingOption> {
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Center(
-          child: const Text('Vote Counter and Reporter'
-              ),
+          child: const Text('Vote Counter and Reporter'),
         ),
       ),
       bottomNavigationBar: AppBottomBar(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton:  AppActionButton(),
+      floatingActionButton: AppActionButton(),
       body: Column(
         children: <Widget>[
-          Card(
-        child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.check_box_outline_blank),
-            title: Text('S.no.: 9475y38'),
-            subtitle:Text('S.no.: 9475y38'),
-            trailing: Text('S.no.: 9475y38')
-          ),
-          ListTile(
-            trailing: FlatButton.icon(
-          onPressed: null,
-          icon: Icon(Icons.pin_drop),
-          label: Text('S.no.: 9475y38')
-            ),),
-            ButtonBar(
-          children: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.share),
-              onPressed: () {
-
-              },
-            ),
-            OutlineButton(
-              child: const Text('Reject'),
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: Text('Reason for Rejection'),
-                      content: TextField(
-                        decoration: InputDecoration(
-                            hintText: "Reason for Rejection"),
-                      ),
-                      actions: <Widget>[
-                        new FlatButton(
-                          child: new Text('Submit'),
-                          onPressed: () {},
-                        )
-                      ],
-                    );
-                  },
-                );
-              },
-            ),
-            RaisedButton(
-              child: Icon(Icons.camera_alt),
-              onPressed: () {
-
-              },
-            ),
-          ],
-            ),
-        ],
-      ),
-    ),
           Card(
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 ListTile(
-                    leading: Icon(Icons.check_box_outline_blank),
-                    title: Text('S.no.: 9475y38'),
-                    subtitle:Text('S.no.: 9475y38'),
-                    trailing: Text('S.no.: 9475y38')
-                ),
+                    leading: Icon(Icons.adjust),
+                    title: Text('General Elections'),
+                    subtitle: Text('UID: 877436677366'),
+                    trailing: Text('Check Details')),
                 ListTile(
-                  trailing: FlatButton.icon(
-                      onPressed: null,
-                      icon: Icon(Icons.pin_drop),
-                      label: Text('S.no.: 9475y38')
-                  ),),
-            ButtonBar(
-          children: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.share),
-              onPressed: () {
-
-              },
-            ),
-            OutlineButton(
-              child: const Text('Reject'),
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: Text('Reason for Rejection'),
-                      content: TextField(
-                        decoration: InputDecoration(
-                            hintText: "Reason for Rejection"),
-                      ),
-                      actions: <Widget>[
-                        new FlatButton(
-                          child: new Text('Submit'),
-                          onPressed: () {},
-                        )
-                      ],
-                    );
-                  },
-                );
-              },
-            ),
-            RaisedButton(
-              child: Icon(Icons.camera_alt),
-              onPressed: () {
-
-              },
-            ),
-          ],
-            ),
+                  leading: Text('Ongoing'),
+                  trailing: Text('1 April 2020')
+                ),
+                ButtonBar(
+                  children: <Widget>[
+                    RaisedButton(
+                      child: Text('Vote'),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
@@ -155,56 +57,44 @@ class _VotingOptionState extends State<VotingOption> {
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 ListTile(
-                    leading: Icon(Icons.check_box_outline_blank),
-                    title: Text('S.no.: 9475y38'),
-                    subtitle:Text('S.no.: 9475y38'),
-                    trailing: Text('S.no.: 9475y38')
-                ),
+                    leading: Icon(Icons.adjust),
+                    title: Text('Mayor Elections'),
+                    trailing: Text('Check Details')),
                 ListTile(
-                  trailing: RaisedButton.icon(
-                      onPressed: null,
-                      icon: Icon(Icons.pin_drop),
-                      label: Text('S.no.: 9475y38')
-                  ),),
-            ButtonBar(
-          children: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.share),
-              onPressed: () {
-
-              },
+                  title: Text('City: Delhi'),
+                  trailing: Text('Due date : 15 April 2020')
+                ),
+                ButtonBar(
+                  children: <Widget>[
+                    RaisedButton(
+                      child: Text(' Due'),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ],
             ),
-            OutlineButton(
-              child: const Text('Reject'),
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: Text('Reason for Rejection'),
-                      content: TextField(
-                        decoration: InputDecoration(
-                            hintText: "Reason for Rejection"),
-                      ),
-                      actions: <Widget>[
-                        new FlatButton(
-                          child: new Text('Submit'),
-                          onPressed: () {},
-                        )
-                      ],
-                    );
-                  },
-                );
-              },
-            ),
-            RaisedButton(
-              child: Icon(Icons.camera_alt),
-              onPressed: () {
-
-              },
-            ),
-          ],
-            ),
+          ),
+          Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ListTile(
+                    leading: Icon(Icons.adjust),
+                    title: Text('State Elections'),
+                    trailing: Text('Check Details')),
+                ListTile(
+                    title: Text('State: Mumbai'),
+                    trailing: Text('Done on: 20 March 2020')
+                ),
+                ButtonBar(
+                  children: <Widget>[
+                    RaisedButton(
+                      child: Text('Check Results'),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
               ],
             ),
           )
